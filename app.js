3ee762c698fc643c1938a20e53e6d1e65965613d
mrmojica <mrmojica510@gmail.com>
2016-07-25 22:11:06 -0700

    cleaned up

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
 
-//List of ingredients
+    //List of ingredients
 
-var ingredients = {
-    strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
-    salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
-    bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
-    sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
-    fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
-}
+    var ingredients = {
+        strong: ['Glug of rum', 'slug of whisky', 'splash of gin'],
+        salty: ['Olive on a stick', 'salt-dusted rim', 'rasher of bacon'],
+        bitter: ['Shake of bitters', 'splash of tonic', 'twist of lemon peel'],
+        sweet: ['Sugar cube', 'spoonful of honey', 'splash of cola'],
+        fruity: ['Slice of orange', 'dash of cassis', 'cherry on top']
+    }
 
-//Constructor function that gets users values
+    //Constructor function that gets users values
 
-var Drink = function(strong, salty, bitter, sweet, fruity) {
-    this.strong = strong;
-    this.salty = salty;
-    this.bitter = bitter;
-    this.sweet = sweet;
-    this.fruity = fruity;
-};
-
-
-
-//When order button is clicked get values.  
-
-$('.orderButton').click(function(event) {
-	event.preventDefault();
-var random = Math.floor(Math.random() * 3);
-var newDrink = new Drink(
-	$('.strongDrink input:checked').val(),
-	$('.saltyDrink input:checked').val(),
-	$('.bitterDrink input:checked').val(),
-	$('.sweetDrink input:checked').val(),
-	$('.fruityDrink input:checked').val());
-
-//Clear drinks
-$('.display-drinks').empty();
-
-
-//If user selected yes - display random drink by user preference
-if (newDrink.strong === 'true') {
-	$('.display-drinks').append('<li>' + ingredients.strong[random] + '</li>');
-
-}
-if (newDrink.salty === 'true') {
-	$('.display-drinks').append('<li>' + ingredients.salty[random] + '</li>');
-}
-if (newDrink.bitter === 'true') {
-	$('.display-drinks').append('<li>' + ingredients.bitter[random] + '</li>');
-}
-if (newDrink.sweet === 'true') {
-	$('.display-drinks').append('<li>' + ingredients.sweet[random] + '</li>');
-}
-if (newDrink.fruity === 'true') {
-	$('.display-drinks').append('<li>' + ingredients.fruity[random] + '</li>');
-}
+    var Drink = function(strong, salty, bitter, sweet, fruity) {
+        this.strong = strong;
+        this.salty = salty;
+        this.bitter = bitter;
+        this.sweet = sweet;
+        this.fruity = fruity;
+    };
 
 
 
-});	
+    //When order button is clicked get values.  
+
+    $('.orderButton').click(function(event) {
+        event.preventDefault();
+        var random = Math.floor(Math.random() * 3);
+        var newDrink = new Drink(
+            $('.strongDrink input:checked').val(),
+            $('.saltyDrink input:checked').val(),
+            $('.bitterDrink input:checked').val(),
+            $('.sweetDrink input:checked').val(),
+            $('.fruityDrink input:checked').val());
+
+        //Clear drinks
+        $('.display-drinks').empty();
+
+
+        //If user selected yes - display random drink by user preference
+        if (newDrink.strong === 'true') {
+            $('.display-drinks').append('<li>' + ingredients.strong[random] + '</li>');
+
+        }
+        if (newDrink.salty === 'true') {
+            $('.display-drinks').append('<li>' + ingredients.salty[random] + '</li>');
+        }
+        if (newDrink.bitter === 'true') {
+            $('.display-drinks').append('<li>' + ingredients.bitter[random] + '</li>');
+        }
+        if (newDrink.sweet === 'true') {
+            $('.display-drinks').append('<li>' + ingredients.sweet[random] + '</li>');
+        }
+        if (newDrink.fruity === 'true') {
+            $('.display-drinks').append('<li>' + ingredients.fruity[random] + '</li>');
+        }
+
+
+
+    });
 
 
 
